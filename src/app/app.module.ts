b027431b62be73cf7e6774374183c723fc6facde
Seetala Gopala Rao  (NCS) <seetalagr@ncs.com.sg>
2017-10-25 18:00:25 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { AboutSkillsComponent } from './about-skills/about-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'about', component: AboutSkillsComponent
+  },
+  {
+    path: 'portfolio', component: PortfolioComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    ContentComponent,
+    AboutSkillsComponent,
+    PortfolioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
