60e0cb24af8321e1545e92c8490b13156220b061
Seetala Gopala Rao  (NCS) <seetalagr@ncs.com.sg>
2017-10-26 12:12:26 +0800

    modified portfolio ngfor

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -8,25 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent  {
-
-  thumbnails =[
-    {id:1, title:"Ministry of Transport"},
-    {id:2, title:"Ministry of Transport - Mobile"},
-    {id:3, title:"Businesses for Families Council"},
-    {id:4, title:"Hella Studio"},
-    {id:5, title:"NUS - Business School"},
-    {id:6, title:"Workplace saftey and health Council"},
-    {id:7, title:"eCitizen"},
-    {id:8, title:"eCitizen - Mobile"},
-    {id:9, title:"Clean & Green"},
-    {id:10, title:"OpenNet"},
-    {id:11, title:"SG Changi"},
-    {id:12, title:"Singapore Civil Defence Force"},
-    {id:13, title:"GVEC"},
-    {id:14, title:"Info Lab"},
-    {id:15, title:"Lemon Tree Group"},
-    {id:16, title:"Humira"}
-    
-    
-  ];
+  thumbnails : any[];    
+  constructor(){
+    this.thumbnails = [
+      {id:1, title:"Ministry of Transport"},
+      {id:2, title:"Ministry of Transport - Mobile"},
+      {id:3, title:"Businesses for Families Council"},
+      {id:4, title:"Hella Studio"},
+      {id:5, title:"NUS - Business School"},
+      {id:6, title:"Workplace saftey and health Council"},
+      {id:7, title:"eCitizen"},
+      {id:8, title:"eCitizen - Mobile"},
+      {id:9, title:"Clean & Green"},
+      {id:10, title:"OpenNet"},
+      {id:11, title:"SG Changi"},
+      {id:12, title:"Singapore Civil Defence Force"},
+      {id:13, title:"GVEC"},
+      {id:14, title:"Info Lab"},
+      {id:15, title:"Lemon Tree Group"},
+      {id:16, title:"Humira"}
+    ]
+  }
 }
